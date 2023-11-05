1670595c9919432cd1e69986ec4b7a7bba8371d4
raulraul <raulvdb@hotmail.com>
2023-11-05 13:39:31 +0100

    toevoeging voetbalspeler

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):   
+class Voetbalspeler(models.Model):   
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    name = models.CharField(max_length=200)
+    club = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -14,4 +14,5 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.name
+
